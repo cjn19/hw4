9a9380f37186f1790dff56cc92ff62d0d1bc3444
Jayanth  Challapalli <jayanth.challapalli@kellogg.northwestern.edu>
2024-06-05 07:02:27 +0000

    FINALLY WORKING - USED "form-date", instead of "form-data" :'(

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,5 +1,4 @@
 class EntriesController < ApplicationController
-
   def index
     @entries = Entry.all
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,6 @@ class UsersController < ApplicationController
     if User.find_by({"email" => params["email"]}) == nil
       @user = User.new
       @user["username"] = params["username"]
-      ##@user["first_name"] = params["first_name"]
-      ##@user["last_name"] = params["last_name"]
       @user["email"] = params["email"]
       @user["password"] = BCrypt::Password.create(params["password"])
       @user.save
